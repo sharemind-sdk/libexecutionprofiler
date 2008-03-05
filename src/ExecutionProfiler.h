2f2503501329fd4d980018710538b32f79c851d2
Dan Bogdanov <dan.bogdanov@cyber.ee>
2008-03-05 09:51:40 +0000

    Changed headers for year 2008
    git-svn-id: https://research.cyber.ee/projects/sharemind/svnroot/trunk@164 10f935d1-c939-0410-8f69-b94c1d3c5283

--- src/ExecutionProfiler.h
+++ src/ExecutionProfiler.h
@@ -1,7 +1,7 @@
 /*
 * This file is a part of the Sharemind framework.
 * 
-* Copyright (C) Dan Bogdanov, 2006-2007
+* Copyright (C) Dan Bogdanov, 2006-2008
 * All rights are reserved. Reproduction in whole or part is prohibited
 * without the written consent of the copyright owner.
 */
