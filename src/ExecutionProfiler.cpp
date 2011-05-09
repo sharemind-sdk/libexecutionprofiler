e2b40f380783b6645468429eb73004507021e1d6
Roman Jagomägis <roman.jagomagis@cyber.ee>
2011-05-09 15:18:11 +0300

    Refactored ExecutionProfiler to support scoped sections. Fixed some compilation issues. Fixed headers.

--- src/ExecutionProfiler.cpp
+++ src/ExecutionProfiler.cpp
@@ -1,19 +1,27 @@
 /*
+/*
  * This file is a part of the Sharemind framework.
+ * Copyright (C) Cybernetica AS
  *
- * Copyright (C) AS Cybernetica
  * All rights are reserved. Reproduction in whole or part is prohibited
- * without the written consent of the copyright owner.
- *
- * Main contributors:
- * Dan Bogdanov (dan@cyber.ee)
+ * without the written consent of the copyright owner. The usage of this
+ * code is subject to the appropriate license agreement.
  */
 
-#include "GetTime.h"
-#include "../common/CommonLibrary.h"
-using namespace std;
+#include <iostream>
 
-ExecutionSection::ExecutionSection(uint16 actionCode, uint32 complexityParameter, uint32 parentSectionId) {
+#include <GetTime.h>
+
+#include "ExecutionProfiler.h"
+#include "Logger.h"
+
+using std::endl;
+using std::ofstream;
+using std::make_pair;
+using std::string;
+using std::map;
+
+ExecutionSection::ExecutionSection(ProfilerActionCode actionCode, uint32 complexityParameter, uint32 parentSectionId) {
 	this->actionCode = actionCode;
 	this->complexityParameter = complexityParameter;
 	this->parentSectionId = parentSectionId;
@@ -25,15 +33,133 @@ ExecutionProfiler::ExecutionProfiler(Logger* logger)
 {
 	m_nextSectionId = 0;
 	m_enableProfiling = false;
-	m_profileLogMutex = new mutex;
-	assert (m_profileLogMutex);
 }
 
 
 ExecutionProfiler::~ExecutionProfiler() {
 	finishLog();
-	if (m_profileLogMutex)
-		delete m_profileLogMutex;
+}
+
+bool ExecutionProfiler::startLog(const string& filename) {
+    m_filename = filename;
+
+    // Lock the list
+    boost::mutex::scoped_lock lock (m_profileLogMutex);
+
+    // Check if we have a filename
+    if (m_filename.length () > 0) {
+
+        // Try to open the log file
+        m_logfile.open (m_filename.c_str ());
+        if (m_logfile.bad() || m_logfile.fail ()) {
+            WRITE_LOG_ERROR (m_logger, "[ExecutionProfiler] ERROR: Can't open logger log file " << m_filename << "!");
+            return false;
+        }
+
+        WRITE_LOG_DEBUG (m_logger, "[ExecutionProfiler] Opened profiling log file " << m_filename << "!");
+
+        {
+//            boost::mutex::scoped_lock lock (m_logger->getStreamMutex());
+            m_logfile << "SectionID" << ", " << "Start" << ", " << "End"
+                      << ", " << "Duration" << ", " << "Action" << ", "
+                      << "Complexity" << ", " << "ParentSectionID" << endl;
+        }
+
+        m_enableProfiling = true;
+        return true;
+
+    } else {
+
+        // We didn't get a filename so spread the information about that.
+        WRITE_LOG_ERROR (m_logger, "[ExecutionProfiler]  ERROR: Empty log file name!");
+        return false;
+    }
+}
+
+void ExecutionProfiler::finishLog() {
+    if (!m_enableProfiling)
+        return;
+
+    // Lock the list
+    boost::mutex::scoped_lock lock (m_profileLogMutex);
+
+    WRITE_LOG_DEBUG (m_logger, "[ExecutionProfiler] Flushing profiling log file.");
+
+    // Flush all sections to the disc
+    while (m_sections.size () > 0) {
+        // Give time in one-second slices
+        ExecutionSection s = m_sections.front ();
+        //WRITE_LOG_FULLDEBUG (m_logger, "[ExecutionProfiler] Logging section " << s.sectionId << ".");
+        {
+//            boost::mutex::scoped_lock lock (m_logger->getStreamMutex());
+            m_logfile << s.sectionId << ", " << s.startTime << ", " << s.endTime
+                      << ", " << (s.endTime - s.startTime) << ", "
+                      << s.actionCode << ", " << s.complexityParameter
+                      << ", " << s.parentSectionId << endl;
+        }
+        m_sections.pop_front ();
+    }
+
+    // Close the log file, if necessary
+    if (m_logfile.is_open ()) {
+        WRITE_LOG_DEBUG (m_logger, "[ExecutionProfiler] Closing log file " << m_filename);
+        m_logfile.close ();
+    }
+}
+
+void ExecutionProfiler::processLog(uint32 timeLimitMs, bool flush) {
+    if (!m_enableProfiling)
+        return;
+
+    // Lock the list
+    boost::mutex::scoped_lock lock (m_profileLogMutex);
+
+    uint32 start = RakNet::GetTime ();
+    uint32 end = start + timeLimitMs;
+
+    uint32 leaveSections = 0;
+
+    while (RakNet::GetTime () < end && m_sections.size () > leaveSections) {
+        ExecutionSection s = m_sections.front ();
+        //WRITE_LOG_FULLDEBUG (m_logger, "[ExecutionProfiler] Logging section " << s.sectionId << ".");
+        {
+//            boost::mutex::scoped_lock lock (m_logger->getStreamMutex());
+            if (m_logfile.is_open ())
+                m_logfile << s.sectionId << ", " << s.startTime << ", "
+                          << s.endTime << ", " << (s.endTime - s.startTime)
+                          << ", " << s.actionCode << ", " << s.complexityParameter
+                          << ", " << s.parentSectionId << endl;
+        }
+        m_sections.pop_front ();
+    }
+}
+
+uint32 ExecutionProfiler::startSection(ProfilerActionCode actionCode, uint32 complexityParameter, uint32 parentSectionId/* = 0 */) {
+    if (!m_enableProfiling)
+        return 0;
+
+    // Lock the list
+    boost::mutex::scoped_lock lock (m_profileLogMutex);
+
+    // Automatically set parent
+    uint32 usedParentSectionId = parentSectionId;
+    if (parentSectionId == 0) {
+        if (m_sectionStack.size () > 0) {
+            usedParentSectionId = m_sectionStack.top ();
+        }
+    }
+
+    // Create the entry and store it
+    ExecutionSection s (actionCode, complexityParameter, usedParentSectionId);
+    s.startTime = RakNet::GetTime ();
+    s.endTime = 0;
+    s.sectionId = m_nextSectionId;
+    m_sectionMap.insert (make_pair (s.sectionId, s));
+    m_nextSectionId++;
+
+    //WRITE_LOG_FULLDEBUG (m_logger, "[ExecutionProfiler] Started section " << s.sectionId << ".");
+
+    return s.sectionId;
 }
 
 void ExecutionProfiler::endSection(uint32 sectionId) {
@@ -41,7 +167,7 @@ void ExecutionProfiler::endSection(uint32 sectionId) {
 		return;
 
 	// Lock the list
-	boost::mutex::scoped_lock lock (*m_profileLogMutex);
+    boost::mutex::scoped_lock lock (m_profileLogMutex);
 
 	map<uint32, ExecutionSection>::iterator it = m_sectionMap.find (sectionId);
 	if (it == m_sectionMap.end ()) {
@@ -54,144 +180,24 @@ void ExecutionProfiler::endSection(uint32 sectionId) {
 	m_sectionMap.erase (it);
 }
 
+void ExecutionProfiler::pushParentSection(uint32 sectionId) {
+    if (!m_enableProfiling)
+        return;
 
-void ExecutionProfiler::finishLog() {
-	if (!m_enableProfiling)
-		return;
-
-	// Lock the list
-	boost::mutex::scoped_lock lock (*m_profileLogMutex);
-
-	WRITE_LOG_DEBUG (m_logger, "[ExecutionProfiler] Flushing profiling log file.");
-
-	// Flush all sections to the disc
-	while (m_sections.size () > 0) {
-		// Give time in one-second slices
-		ExecutionSection s = m_sections.front ();
-		//WRITE_LOG_FULLDEBUG (m_logger, "[ExecutionProfiler] Logging section " << s.sectionId << ".");
-		{
-//            boost::mutex::scoped_lock lock (m_logger->getStreamMutex());
-            m_logfile << s.sectionId << ", " << s.startTime << ", " << s.endTime << ", " << (s.endTime - s.startTime) << ", " << s.actionCode << ", " << s.complexityParameter << ", " << s.parentSectionId << endl;
-		}
-		m_sections.pop_front ();
-	}
-
-	// Close the log file, if necessary
-	if (m_logfile.is_open ()) {
-		WRITE_LOG_DEBUG (m_logger, "[ExecutionProfiler] Closing log file " << m_filename);
-		m_logfile.close ();
-	}
+    // Lock the list
+    boost::mutex::scoped_lock lock (m_profileLogMutex);
+    m_sectionStack.push (sectionId);
 }
-
 
 void ExecutionProfiler::popParentSection() {
 	if (!m_enableProfiling)
 		return;
 
 	// Lock the list
-	boost::mutex::scoped_lock lock (*m_profileLogMutex);
+    boost::mutex::scoped_lock lock (m_profileLogMutex);
 
 	if (m_sectionStack.size () > 0)
 		m_sectionStack.pop ();
-}
-
-
-void ExecutionProfiler::processLog(uint32 timeLimitMs, bool flush) {
-	if (!m_enableProfiling)
-		return;
-
-	// Lock the list
-	boost::mutex::scoped_lock lock (*m_profileLogMutex);
-
-	uint32 start = RakNet::GetTime ();
-	uint32 end = start + timeLimitMs;
-
-	uint32 leaveSections = 0;
-
-	while (RakNet::GetTime () < end && m_sections.size () > leaveSections) {
-		ExecutionSection s = m_sections.front ();
-		//WRITE_LOG_FULLDEBUG (m_logger, "[ExecutionProfiler] Logging section " << s.sectionId << ".");
-		{
-//            boost::mutex::scoped_lock lock (m_logger->getStreamMutex());
-			if (m_logfile.is_open ())
-				m_logfile << s.sectionId << ", " << s.startTime << ", " << s.endTime << ", " << (s.endTime - s.startTime) << ", " << s.actionCode << ", " << s.complexityParameter << ", " << s.parentSectionId << endl;
-		}
-		m_sections.pop_front ();
-	}
-}
-
-
-void ExecutionProfiler::pushParentSection(uint32 sectionId) {
-	if (!m_enableProfiling)
-		return;
-
-	// Lock the list
-	boost::mutex::scoped_lock lock (*m_profileLogMutex);
-	m_sectionStack.push (sectionId);
-}
-
-
-bool ExecutionProfiler::startLog(const string& filename) {
-	m_filename = filename;
-
-	// Lock the list
-	boost::mutex::scoped_lock lock (*m_profileLogMutex);
-
-	// Check if we have a filename
-	if (m_filename.length () > 0) {
-
-		// Try to open the log file
-		m_logfile.open (m_filename.c_str ());
-		if (m_logfile.bad() || m_logfile.fail ()) {
-			WRITE_LOG_ERROR (m_logger, "[ExecutionProfiler] ERROR: Can't open logger log file " << m_filename << "!");
-			return false;
-		}
-
-		WRITE_LOG_DEBUG (m_logger, "[ExecutionProfiler] Opened profiling log file " << m_filename << "!");
-
-        {
-//            boost::mutex::scoped_lock lock (m_logger->getStreamMutex());
-            m_logfile << "SectionID" << ", " << "Start" << ", " << "End" << ", " << "Duration" << ", " << "Action" << ", " << "Complexity" << ", " << "ParentSectionID" << endl;
-        }
-
-		m_enableProfiling = true;
-		return true;
-
-	} else {
-
-		// We didn't get a filename so spread the information about that.
-		WRITE_LOG_ERROR (m_logger, "[ExecutionProfiler]  ERROR: Empty log file name!");
-		return false;
-	}
-}
-
-
-uint32 ExecutionProfiler::startSection(uint16 actionCode, uint32 complexityParameter, uint32 parentSectionId/* = 0 */) {
-	if (!m_enableProfiling)
-		return 0;
-
-	// Lock the list
-	boost::mutex::scoped_lock lock (*m_profileLogMutex);
-
-	// Automatically set parent
-	uint32 usedParentSectionId = parentSectionId;
-	if (parentSectionId == 0) {
-		if (m_sectionStack.size () > 0) {
-			usedParentSectionId = m_sectionStack.top ();
-		}
-	}
-
-	// Create the entry and store it
-	ExecutionSection s (actionCode, complexityParameter, usedParentSectionId);
-	s.startTime = RakNet::GetTime ();
-	s.endTime = 0;
-	s.sectionId = m_nextSectionId;
-	m_sectionMap.insert (make_pair (s.sectionId, s));
-	m_nextSectionId++;
-
-	//WRITE_LOG_FULLDEBUG (m_logger, "[ExecutionProfiler] Started section " << s.sectionId << ".");
-
-	return s.sectionId;
 }
 
 void ExecutionProfiler::logInstructionTime (const string& name, uint32 time) {
@@ -203,11 +209,24 @@ void ExecutionProfiler::logInstructionTime (const string& name, uint32 time) {
 }
 
 void ExecutionProfiler::dumpInstructionTimings (const string& filename) {
-	ofstream f (filename.c_str());
+    ofstream f (filename.c_str());
 	f << "Op\tTime" << endl;
 	BOOST_FOREACH(timingmap::value_type i, m_instructionTimings) {
 		f << i.first<< "\t" <<i.second<< endl;
 	}
 	f.close ();
 	WRITE_LOG_NORMAL(m_logger, "Logged script execution profile to " << filename << ".");
+}
+
+ExecutionSectionScope::ExecutionSectionScope(ExecutionProfiler* profiler, uint32 sectionId, bool isParent) {
+    m_profiler = profiler;
+    m_sectionId = sectionId;
+    m_isParent = isParent;
+}
+
+ExecutionSectionScope::~ExecutionSectionScope() {
+    if (m_isParent) {
+        POP_PARENT_SECTION(m_profiler)
+    }
+    END_SECTION(m_profiler, m_sectionId)
 }
--- src/ExecutionProfiler.h
+++ src/ExecutionProfiler.h
@@ -1,15 +1,14 @@
 /*
  * This file is a part of the Sharemind framework.
+ * Copyright (C) Cybernetica AS
  *
- * Copyright (C) AS Cybernetica
  * All rights are reserved. Reproduction in whole or part is prohibited
- * without the written consent of the copyright owner.
- *
- * Main contributors:
- * Dan Bogdanov (dan@cyber.ee)
+ * without the written consent of the copyright owner. The usage of this
+ * code is subject to the appropriate license agreement.
  */
-#ifndef EXECUTIONPROFILER_H
-#define EXECUTIONPROFILER_H
+
+#ifndef SHAREMINDCOMMON_EXECUTIONPROFILER_H
+#define SHAREMINDCOMMON_EXECUTIONPROFILER_H
 
 #include <stack>
 #include <deque>
@@ -21,14 +20,9 @@
 #include <boost/foreach.hpp>
 #include <boost/unordered_map.hpp>
 #include <fstream>
-using std::ofstream;
-using boost::unordered_map;
-using std::stack;
-using std::map;
-using std::deque;
-using std::ofstream;
-using std::string;
-using boost::mutex;
+
+#include "common/SharemindTypes.h"
+#include "common/ProfilerActionCode.h"
 
 class Logger;
 
@@ -37,145 +31,66 @@ typedef boost::unordered_map<std::string, int> timingmap;
 #define PROFILE_APP
 #define PROFILE_VM
 
+// common profiling defines
 #if defined(PROFILE_APP) || defined(PROFILE_VM)
-	#define PUSH_PARENT_SECTION(profiler, section)\
-		if (profiler) (profiler)->pushParentSection(section);
+    #define START_SECTION(profiler, sid, type, parameter)\
+        uint32 sid = 0; if (profiler) sid = (profiler)->startSection (type, parameter);
+    #define END_SECTION(profiler, sid)\
+        if (profiler) (profiler)->endSection(sid);
+
+    #define PUSH_PARENT_SECTION(profiler, sid)\
+        if (profiler) (profiler)->pushParentSection(sid);
 	#define POP_PARENT_SECTION(profiler)\
 		if (profiler) (profiler)->popParentSection();
 	#define PROCESS_SECTIONS(profiler, time)\
 		if (profiler) (profiler)->processLog(time);
+
+    #define SCOPED_SECTION(profiler, sid, type, parameter)\
+        START_SECTION(profiler, sid, type, parameter)\
+        PUSH_PARENT_SECTION(profiler, sid)\
+        ExecutionSectionScope sectionScope_##sid(profiler, sid, true);
 #else
-	#define PUSH_PARENT_SECTION(profiler, section)
+    #define START_SECTION(profiler, sid, type, parameter)
+    #define END_SECTION(profiler, sid)
+    #define PUSH_PARENT_SECTION(profiler, sid)
 	#define POP_PARENT_SECTION(profiler)
 	#define PROCESS_SECTIONS(profiler, time)
+    #define SCOPED_SECTION(profiler, sid, type, parameter)
 #endif
 
+// vm specific profiling defines
 #ifdef PROFILE_VM
-	#define START_SECTION_VM(profiler, x, type, parameter)\
-		uint32 x = 0; if (profiler) x = (profiler)->startSection (type, parameter);
-	#define END_SECTION_VM(profiler, x)\
-		if (profiler) (profiler)->endSection(x);
+    #define START_SECTION_VM(profiler, sid, type, parameter)\
+        START_SECTION(profiler, sid, type, parameter)
+    #define END_SECTION_VM(profiler, sid)\
+        END_SECTION(profiler, sid)
 #else
-	#define START_SECTION_VM(profiler, x, type, parameter)
-	#define END_SECTION_VM(profiler, x)
+    #define START_SECTION_VM(profiler, sid, type, parameter)
+    #define END_SECTION_VM(profiler, sid)
 #endif
 
+// application specific profiling defines
 #ifdef PROFILE_APP
-	#define START_SECTION_APP(profiler, x, type, parameter)\
-		uint32 x = 0; if (profiler) x = (profiler)->startSection (type, parameter);
-	#define END_SECTION_APP(profiler, x)\
-		if (profiler) (profiler)->endSection(x);
-    #define START_CODE_TIMER(x,label)\
-            uint32 x##Timer##label = 0; x##Timer##label = RakNet::GetTime ();
-    #define UPDATE_CODE_TIMER(x,label)\
-            x += RakNet::GetTime () - x##Timer##label;
-    #define END_CODE_TIMER(profiler, x, codename)\
-            (profiler)->logInstructionTime (codename, x);
-	#define START_INSTRUCTION_TIMER(profiler, x, instruction)\
+    #define START_SECTION_APP(profiler, sid, type, parameter)\
+        START_SECTION(profiler, sid, type, parameter)
+    #define END_SECTION_APP(profiler, sid)\
+        END_SECTION(profiler, sid)
+
+    #define START_INSTRUCTION_TIMER(profiler, x, instruction)\
 		uint32 x = 0; if (profiler) x = RakNet::GetTime ();
 	#define END_INSTRUCTION_TIMER(profiler, x)\
 		if (profiler) (profiler)->logInstructionTime (instructionToExecute->OpName(), RakNet::GetTime() - x);
 	#define DUMP_INSTRUCTION_TIMINGS(profiler, filename)\
 		if (profiler) (profiler)->dumpInstructionTimings (filename);
 #else
-	#define START_SECTION_APP(profiler, x, type, parameter)
-	#define END_SECTION_APP(profiler, x)
+    #define START_SECTION_APP(profiler, sid, type, parameter)
+    #define END_SECTION_APP(profiler, sid)
+
 	#define START_INSTRUCTION_TIMER(profiler, x, instruction)
 	#define END_INSTRUCTION_TIMER(profiler, x)
 	#define DUMP_INSTRUCTION_TIMINGS(profiler, filename)
 #endif
 
-
-
-/*! The codes describing actions performed in an execution section.
-They are specified numerically, because the ProfileLogAnalyst tool
-relies on these numbers right now. */
-enum ActionCodes {
-
-	// NetworkNode actions
-
-	/*! The time spent to receive strings */
-	ACTION_RECEIVE_STRING = 1,
-
-	/*! The time spent to receive values */
-	ACTION_RECEIVE_VALUE = 2,
-
-	/*! The time spent to receive value vectors */
-	ACTION_RECEIVE_VALUE_VECTOR = 3,
-
-	/*! The time spent to flush outgoing network messages */
-	ACTION_FLUSH_NETWORK = 4,
-
-	/*! The time spent to send messages in the controller sessions' outgoing queues */
-	ACTION_PROCESS_OUTGOING_QUEUES = 5,
-
-	/*! The time spent to process and distribute incoming messages */
-	ACTION_PROCESS_INCOMING_PACKETS = 6,
-
-	/*! The time spent in the data saving synchronization protocol */
-	ACTION_PROTOCOL_DATASAVING = 7,
-
-	/*! The time spent in the test protocol */
-	ACTION_PROTOCOL_TESTING = 8,
-
-	/*! The time spent in the bit extraction protocol */
-	ACTION_PROTOCOL_VECTORIZED_ADDITION = 9,
-
-	/*! The time spent in the bit extraction protocol */
-	ACTION_PROTOCOL_VECTORIZED_BITEXTRACTION = 10,
-
-	/*! The time spent in the bitwise addition protocol */
-	ACTION_PROTOCOL_VECTORIZED_BITWISEADDITION = 11,
-
-	/*! The time spent in the share conversion protocol */
-	ACTION_PROTOCOL_VECTORIZED_SHARECONVERSION = 12,
-
-	/*! The time spent in the multiplication protocol */
-	ACTION_PROTOCOL_VECTORIZED_MULTIPLICATION = 13,
-
-	/*! The time spent in the division protocol */
-	ACTION_PROTOCOL_VECTORIZED_DIVISION = 14,
-
-	/*! The time spent in the division protocol */
-	ACTION_PROTOCOL_VECTORIZED_DIVISION_BY_PUBLIC = 15,
-
-	/*! The time spent in the remainder computation protocol */
-	ACTION_PROTOCOL_VECTORIZED_REMAINDER = 16,
-
-	/*! The time spent in the multiplication protocol */
-	ACTION_PROTOCOL_VECTORIZED_SHIFTRIGHT = 17,
-
-	/*! The whole lifespan of a vector equality comparison protocol */
-	ACTION_PROTOCOL_VECTORIZED_EQUALITYCOMPARISON = 18,
-
-	/*! The whole lifespan of a vector greater-than comparison protocol */
-	ACTION_PROTOCOL_VECTORIZED_GREATERTHANCOMPARISON = 19,
-
-	/*! The whole lifespan of a vector greater-than comparison protocol */
-	ACTION_PROTOCOL_MATRIX_SHUFFLE = 20,
-
-	/*! The time spent in the apriori optimized protocol */
-	ACTION_PROTOCOL_APRIORI_OPTIMIZED = 21,
-
-	/*! Randomness generation */
-	ACTION_RANDOMNESS_GENERATION = 22,
-
-	/*! The execution of a script */
-	ACTION_SCRIPT_EXECUTION = 23,
-
-	/*! Various database I/O operations */
-	ACTION_DATABASE_IO = 24,
-
-	/*! Sharing and publishing of values within the VM */
-	ACTION_SHARING_AND_PUBLISHING = 25,
-
-	ACTION_VECTOR_MANAGEMENT = 26,
-
-	ACTION_EXPRESSION_MANAGEMENT = 27,
-
-	ACTION_EXPRESSION_EVALUATION = 28
-
-};
 
 /**
  This is a data structure for storing executed sections for profiling purposes.
@@ -189,26 +104,16 @@ public:
 	/**
 	 Constructs an execution section based on the given parameters
 
-	 \param[in] actionCode a code describing what the section does (\see the ActionCodes enum)
+     \param[in] actionCode a code describing what the section does (\see the ProfilerActionCode enum)
 	 \param[in] complexityParameter a number describing the O(n) complexity of the section
 	 \param[in] parentSectionId the identifier of a section which contains this one
 	*/
-	ExecutionSection(uint16 actionCode, uint32 complexityParameter, uint32 parentSectionId);
+    ExecutionSection(ProfilerActionCode actionCode, uint32 complexityParameter, uint32 parentSectionId);
 
 	/**
 	 Stores the action code of the section
 	*/
-	uint16 actionCode;
-
-	/**
-	 The O(n) complexity parameter for the section
-	*/
-	uint32 complexityParameter;
-
-	/**
-	 A timestamp for the moment the section was completed
-	*/
-	uint32 endTime;
+    ProfilerActionCode actionCode;
 
 	/**
 	 The identifier of this section
@@ -224,6 +129,16 @@ public:
 	 A timestamp for the moment the section started
 	*/
 	uint32 startTime;
+
+    /**
+     A timestamp for the moment the section was completed
+    */
+    uint32 endTime;
+
+    /**
+     The O(n) complexity parameter for the section
+    */
+    uint32 complexityParameter;
 
 };
 
@@ -254,7 +169,7 @@ public:
 	 \retval true if opening the file was successful
 	 \retval false if opening the file failed
 	*/
-	bool startLog(const string &filename);
+    bool startLog(const std::string &filename);
 
 	/**
 	 Specifies the starting point of a code section for profiling.
@@ -268,7 +183,7 @@ public:
 
 	 \returns an unique identifier for the profiled code section which should be passed to EndSection later on
 	*/
-	uint32 startSection(uint16 actionCode, uint32 complexityParameter, uint32 parentSectionId = 0);
+    uint32 startSection(ProfilerActionCode actionCode, uint32 complexityParameter, uint32 parentSectionId = 0);
 
 	/**
 	 Completes the specified section.
@@ -281,7 +196,7 @@ public:
 	void endSection(uint32 sectionId);
 
 	/**
-	 Finishes profiling and writes or cached section to the log file.
+     Finishes profiling and writes cached section to the log file.
 	*/
 	void finishLog();
 
@@ -320,9 +235,9 @@ public:
 	*/
 	void popParentSection();
 
-	void logInstructionTime (const string& name, uint32 time);
+    void logInstructionTime (const std::string& name, uint32 time);
 
-	void dumpInstructionTimings (const string& filename);
+    void dumpInstructionTimings (const std::string& filename);
 
 private:
 
@@ -331,34 +246,34 @@ private:
 	timingmap m_instructionTimings;
 
 
+    /**
+     The name of the logfile to use
+    */
+    std::string m_filename;
+
 	/**
 	 Handle of the file we write the profiling log to
 	*/
-	ofstream m_logfile;
+    std::ofstream m_logfile;
 
 	/**
 	 The stack of parent section identifiers.
 
-	 \see PushParentSection
+     \see PushParentSection
 	*/
-	stack<uint32> m_sectionStack;
+    std::stack<uint32> m_sectionStack;
 
 	/**
 	 The map of execution sections
 
 	 \see PushParentSection
 	 */
-	map<uint32, ExecutionSection> m_sectionMap;
-
-	/**
-	 The name of the logfile to use
-	*/
-	string m_filename;
+    std::map<uint32, ExecutionSection> m_sectionMap;
 
 	/**
 	 The cache of sections waiting for flushing to the disk
 	*/
-	deque<ExecutionSection> m_sections;
+    std::deque<ExecutionSection> m_sections;
 
 	/**
 	 The next available section identifier
@@ -368,7 +283,7 @@ private:
 	/**
 	 The lock for the profiling log
 	*/
-	mutex* m_profileLogMutex;
+    boost::mutex m_profileLogMutex;
 
 	/**
 	 True, if we have profiling enabled
@@ -377,4 +292,41 @@ private:
 
 };
 
-#endif // EXECUTIONPROFILER_H
+/**
+ This class is used to automatically end ExecutionProfile sections and pop
+ parent sections if an instance of this class goes out of scope.
+*/
+class ExecutionSectionScope {
+
+public:
+    /**
+     Constructs the scope instance for the given section.
+
+     \param[in] sectionId the section id
+     \param[in] isParent is it parent and should it be popped.
+    */
+    ExecutionSectionScope(ExecutionProfiler* profiler, uint32 sectionId, bool isParent);
+
+    /**
+     Ends the section and possibly pops the parent section
+    */
+    ~ExecutionSectionScope();
+
+private:
+    /**
+     The identifier of the section to end.
+    */
+    uint32 m_sectionId;
+
+    /**
+     Indicates whether the section is parent section and should be popped.
+    */
+    bool m_isParent;
+
+    /**
+      Holds the pointer to the ExecutionProfiler instance
+    */
+    ExecutionProfiler* m_profiler;
+};
+
+#endif // SHAREMINDCOMMON_EXECUTIONPROFILER_H
